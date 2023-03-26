385bd0d290b2fcadfd367d8fff2fcaf8acadc831
Vanshika Agrawal <94245786+AgrVanshika@users.noreply.github.com>
2023-03-26 19:33:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ if question != "":
                     },
                     {"role": "user", "content": question},
                     {"role": "user", "content": "Who is the creator of Arachne ?"},
-                    {"role": "assistant", "content": "Arachne was built by a PhD student named Oliver Rodrigues at NJIT"}
+                    {"role": "assistant", "content": "Arachne was built by a PhD student named Oliver Alvarado Rodriguez at NJIT"},
+                    {"role": "assistant", "content": "Arkouda was developed by William Reus and Michael Merrill"}
                 ]
         )
         st.success("Done!")
